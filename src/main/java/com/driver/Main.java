d4d8960daaefc065228afa004c893d439bf1127d
DELL <ankitkumaar1302@gmail.com>
2023-08-26 14:56:30 +0530

    Overridding_Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -15,10 +15,10 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        B b = new B();
+        b.meth();
+
         B obj = new B();
         obj.meth();
-
-        B obj1 = new B();
-        obj1.meth();
     }
 }
